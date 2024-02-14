7ac05411ca64fb0037341be0b0d69f3fe331e9fc
YohannPavone <yohann.pavone@oclock.school>
2024-02-14 14:46:13 +0100

    demarches_homepages

--- Entraidant/src/pages/Demarches/Demarches.jsx
+++ Entraidant/src/pages/Demarches/Demarches.jsx
@@ -1,7 +1,23 @@
+import { useNavigate } from 'react-router-dom';
+import './Demarches_module.scss'; // Assurez-vous que ce chemin est correct
+
 function Demarches() {
+  const navigateTo = useNavigate();
+
+  const redirectToAnotherPage = () => {
+    navigateTo("/autre-page");
+  };
+
   return ( 
-    <h1>Démarches en construction ...</h1>
-   );
+    <div className="container">
+      <div className="button-container">
+        <button type="button" onClick={redirectToAnotherPage}>Demarches administratives</button>
+      </div>
+      <div className="button-container">
+        <button type="button" onClick={redirectToAnotherPage}>Aide financière</button>
+      </div>
+    </div>
+  );
 }
 
 export default Demarches;
